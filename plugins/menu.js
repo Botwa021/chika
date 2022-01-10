7cfec36dbe618fb9eac9660d9dfb105108fb0d9b
games-wabot <97183746+Botwa021@users.noreply.github.com>
2022-01-10 20:14:22 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -7,7 +7,7 @@ let moment = require('moment-timezone')
 const defaultMenu = {
   before: `
 ┌─〔 %me 〕
-├ Hai, %name!
+├ Alo kak, %name!
 │
 ├ Tersisa *%limit Limit*
 ├ Role *%role*
@@ -265,7 +265,8 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), '© Mei Cans', 'Pemilik Bot', `${_p}owner`, 'Donasi', `${_p}donasi`, m)
+    await
+await conn.fakeReply(m.chat, 'Loading...', '0@s.whatsapp.net', 'BY MURSID (+6288233832771)', 'status@broadcast') conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), '© Chika cantik', 'Pemilik Bot', `${_p}owner`, 'Donasi', `${_p}donasi`, m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
