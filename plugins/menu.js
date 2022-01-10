04e390255c416b8ce8b65292a85ba96e4fd0a2b7
games-wabot <97183746+Botwa021@users.noreply.github.com>
2022-01-10 22:33:51 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -265,7 +265,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), '© Mei Cans', 'Pemilik Bot', `${_p}owner`, 'Donasi', `${_p}donasi`, m)
+    await conn.fakeReply(m.chat, 'Loading...', '0@s.whatsapp.net', 'By Mursid (+6288233832771)', 'status@broadcast') conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), '© Chika cantik', 'Pemilik Bot', `${_p}owner`, 'Donasi', `${_p}donasi`, m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
